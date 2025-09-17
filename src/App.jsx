31abe0d207d31da1a60f9e02b72c4406f3a7cc97
tromvn <tomas.bazan@genstudents.org>
2025-09-17 09:26:11 -0300

    Tarea entregada

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
           <Article />
           <Article />
           <Article />
+          <Article />
         </main>
         <aside className="sidebar">
           <AsideDiv />
